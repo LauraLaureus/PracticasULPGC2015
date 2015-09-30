d932713c52521a06a0f92e7931c38883fd3a1893
Laura <l4depende@gmail.com>
2015-09-30 13:31:28 +0100

    funcionaaaaaaaa

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -2,23 +2,21 @@
 using System.Collections;
 
 public class PlayerControler : MonoBehaviour {
-	//private Rigidbody rb;
+	private Rigidbody rb;
 	public float rotationX;
+	public float playerSpeed;
 
 	// Use this for initialization
 	void Start () {
-	//	rb = GetComponent<Rigidbody> ();
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
 		this.transform.eulerAngles = this.transform.eulerAngles + new Vector3(0f, Input.GetAxis("Horizontal")*rotationX,0f);
-		this.transform.position =  this.transform.position + Input.GetAxis ("Vertical") * transform.forward;
-		//float moveVertical = Input.GetAxis ("Vertical"); //transform.foward
-
-		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
+		//this.transform.position =  this.transform.position + Input.GetAxis ("Vertical") * transform.forward;
+		rb.velocity = Input.GetAxis ("Vertical") * transform.forward * playerSpeed;
 	}
 
 }
