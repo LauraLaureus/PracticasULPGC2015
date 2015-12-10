434277dfb7abdbbe406624e6490b015508624d2f
Laura <l4depende@gmail.com>
2015-12-10 13:24:48 +0000

    Doors up and down

--- Assets/DestructuveDoorBehaviour.cs
+++ Assets/DestructuveDoorBehaviour.cs
@@ -3,12 +3,11 @@ using System.Collections;
 
 public class DestructuveDoorBehaviour : MonoBehaviour {
 
-	public float secondsToChangeState = 1;
+	public float secondsToChangeState = 3;
 	public float maxHeight = 10;
 
 	private DoorStatus state = DoorStatus.Lift;
 	private Rigidbody rb;
-	private float f;
 
 	public enum DoorStatus{
 		Lift,
@@ -16,36 +15,28 @@ public class DestructuveDoorBehaviour : MonoBehaviour {
 	}
 
 	void Start () {
-		f = transform.position.y;
 		rb = gameObject.GetComponent<Rigidbody> ();
 		rb.isKinematic = true;
-		StartCoroutine("FSM");
+		rb.useGravity = false;
 	}
 
-	IEnumerator FSM(){
-		while (true) {
-			StartCoroutine (state.ToString ());
-		}
+
+	void FixedUpdate(){
+		Debug.Log("flag");
+		StartCoroutine (state.ToString ());
 	}
 
 	IEnumerator Lift(){
-		Debug.Log ("flag");
-		rb.useGravity = false;
 		while(gameObject.transform.position.y < maxHeight){
-			gameObject.transform.Translate(0,1*Time.deltaTime,0);
+			gameObject.transform.Translate(new Vector3(0,(float)0.1*Time.fixedDeltaTime,0));
 		}
 		state = DoorStatus.Drop;
 		yield return new WaitForSeconds(secondsToChangeState);
-
-
 	}
 
 	IEnumerator Drop(){
-		rb.useGravity = true;
-		RaycastHit hit;
-		Physics.Raycast (transform.position, -transform.up,out hit);
-		while ((hit.point - transform.position).magnitude <0.01f) {
-			Physics.Raycast (transform.position, -transform.up,out hit);
+		while(gameObject.transform.position.y > 0){
+			gameObject.transform.Translate(new Vector3(0,(float)-0.1*Time.fixedDeltaTime,0));
 		}
 		state = DoorStatus.Lift;
 		yield return new WaitForSeconds(secondsToChangeState);
